463cd2e9a41fbd1b5172b30d96620051734404ea
MateuszO99 <56407381+MateuszO99@users.noreply.github.com>
2020-03-20 07:54:46 +0100

    Add files via upload

---
+++ zadanie1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int znajdz_najmniejsza(const vector<int> & v){
+    if (v.empty()){
+        return INT_MIN;
+    }
+
+    int min = v[0];
+    int size = v.size();
+
+    for (int i = 1; i < size; i++){
+        if (min > v[i]){
+            min = v[i];
+        }
+    }
+
+    return min;
+}
+
+
+int main()
+{
+    vector<int> v;
+    v = {12, 1, -7, 44, 0, -8, 0};
+
+    cout << znajdz_najmniejsza(v) << endl;
+
+
+    return 0;
+}
