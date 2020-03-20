d1fb742b63e1de9a01a93b959199dc4eb904e7a2
MateuszO99 <56407381+MateuszO99@users.noreply.github.com>
2020-03-20 08:04:32 +0100

    zadanie2.cpp

---
+++ rozdział3/zadanie2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void zeruj(vector<double> & v){
+    int size = v.size();
+    for (int i = 0; i < size; i++)
+        v[i] *= 0;
+}
+
+int main()
+{
+    const int size = 10;
+    vector<double> v(size);
+
+    for (int i = 0; i < size; i++){
+        v[i] = static_cast<double>(i+1);
+        cout << v[i] << endl;
+    }
+
+    zeruj(v);
+    cout << "Wektor został wyzerowany" << endl;
+
+    for (int i = 0; i < size; i++){\
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
