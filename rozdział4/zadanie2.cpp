c40d29b6b5503166965a2f38d000ebcc0bd30bcc
Mateusz Or <315352@uwr.edu.pl>
2020-04-07 08:32:06 +0200

    Rozdział 4

---
+++ rozdział4/zadanie2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    double (*tablica1)[100];
+    char * tablica2[5][5];
+    void f(char *tab[], size_t size);
+    vector<void*> u;
+    vector<double> tablica3[4];
+    double (*fun)(double[], int);
+    void fun2(int*& arg);
+
+    return 0;
+}
