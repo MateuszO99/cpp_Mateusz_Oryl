be87ed8b9f29ffcc238319d2522cb25bbd8b2f10
Mateusz Or <315352@uwr.edu.pl>
2020-05-21 09:06:17 +0200

    Rozdział 7

---
+++ rozdział7/zadanie2/analogclock.h
@@ -0,0 +1,19 @@
+#ifndef ANALOGCLOCK_H
+#define ANALOGCLOCK_H
+
+#include <QWidget>
+
+class AnalogClock : public QWidget
+{
+    Q_OBJECT
+
+public:
+    AnalogClock(QWidget *parent = 0);
+
+protected:
+    void paintEvent(QPaintEvent *event);
+    virtual void enterEvent(QEvent *event);
+    virtual void leaveEvent(QEvent *event);
+};
+
+#endif // ANALOGCLOCK_H
